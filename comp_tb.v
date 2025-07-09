165d94f36d0efcdf792bcdc0352a85861e033986
Mage-imm <2260.stkabirnrnp@gmail.com>
2025-07-09 19:54:49 +0530

    removed com

--- comp_tb.v
+++ comp_tb.v
@@ -6,14 +6,14 @@ module testbench;
 
     comp uut(.clk(clk), .reset(reset));
 
-    // Clock toggles every 5ns = 10ns period
+  
     always #5 clk = ~clk;
 
     initial begin
-        $dumpfile("wave.vcd");        // for GTKWave
-        $dumpvars(0, testbench);      // dump everything under testbench
+        $dumpfile("wave.vcd");        
+        $dumpvars(0, testbench);      
 
-        #10 reset = 0;                // release reset after 10ns
-        #200 $finish;                 // run simulation for 200ns
+        #10 reset = 0;                
+        #200 $finish;                 
     end
 endmodule
